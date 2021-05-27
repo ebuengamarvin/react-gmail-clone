6ff2f3fff2ba7eab94d256c280dbb6e23c777e24
Marvin Ebuenga <marvinebuenga.cs@gmail.com>
2021-05-27 17:47:55 +0800

    Make routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AppContainer, AppBody } from './styles';
+import Header from 'components/Header';
+import Sidebar from 'components/Sidebar';
+import SendMail from 'components/SendMail';
+import EmailList from 'components/EmailList';
+import Mail from 'components/Mail';
+import { useSelector } from 'react-redux';
+import { selectSendMessageIsOpen } from 'features/mailSlice';
 
 function App() {
+  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+
   return (
     <div>
-      <h1>React</h1>
+      <Router>
+        <AppContainer>
+          <Header />
+
+          <AppBody>
+            <Sidebar />
+            <Switch>
+              <Route exact path="/">
+                <EmailList />
+              </Route>
+              <Route exact path="/mail">
+                <Mail />
+              </Route>
+            </Switch>
+          </AppBody>
+
+          {sendMessageIsOpen && <SendMail />}
+        </AppContainer>
+      </Router>
     </div>
   );
 }
---
+++ src/app/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  height: 100vh;
+`;
+
+export const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
